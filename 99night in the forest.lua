36fdfdb9336d78af36ba72cb8fa4c43007afe500
Zennakub123456789 <typedelegant@gmail.com>
2025-10-16 17:41:18 +0700

    Update MacUI loading URL in 99night in the forest.lua

--- 99night in the forest.lua
+++ 99night in the forest.lua
@@ -1,4 +1,4 @@
-local MacUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zennakub123456789/MacOS/refs/heads/main/Main(1)_1760432973040(1).lua"))()
+local MacUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zennakub123456789/Apple-Library/refs/heads/main/Main.lua"))()
 
 local imageUrl = "https://raw.githubusercontent.com/Zennakub123456789/picture/main/TadHub-Icon.png"
 local imageName = "TadHub-Icon.png"
